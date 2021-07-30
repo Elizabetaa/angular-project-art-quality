d46ec3b8b86182e057c77dbc0941b5e9f04c4301
Elizabetaa <elizabetaa99@abv.bg>
2021-07-30 12:01:40 +0300

    add new user in db

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  constructor(
+    private userService: UserServiceService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient
 
-  constructor() { }
+  ) {}
 
-  ngOnInit(): void {
+  registerHandler(form: NgForm) : void{
+    this.http.post(`http://localhost:8080/users/register`, JSON.stringify(form.value)).subscribe();
+    const redirectUrl = this.activatedRoute.snapshot.queryParams.redirectUrl || '/login';
+    this.router.navigate([redirectUrl]);
   }
-
 }
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
         component: RegisterComponent
     },
     {
-      path: 'signIn',
+      path: 'login',
       component: SignInComponent
   }
 ];
