2e918f22200c029190fc631faafb2d7ddd625f44
Elizabetaa <elizabetaa99@abv.bg>
2021-07-30 16:06:30 +0300

    simple changes for refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksComponent } from './books/books.component';
-import { CoursesComponent } from './courses/courses/courses.component';
 import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
